5275578da233c8ffa4e60534501c66c66c7ec7f4
rickardfjeldseth <rickard.fjeldseth@gmail.com>
2013-02-13 17:34:10 +0100

    La till nyhetsgrejerna

---
+++ studentapp/HVNewsViewController.h
@@ -0,0 +1,15 @@
+//
+//  HVNewsViewController.h
+//  studentapp
+//
+//  Created by Rickard Fjeldseth on 2013-02-13.
+//  Copyright (c) 2013 Högskolan Väst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVNewsViewController : UITableViewController
+{
+    NSArray *items;
+}
+@end
